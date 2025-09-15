aa3326d209fe2bd3ea13680b538fe7b9716ffee7
naveen reddy <naveen reddy@LAPTOP-3F2TIK9B>
2025-09-14 22:07:35 -0500

    grdhtjfdh

--- AspireApp1/AspireApp1.ApiService/Program.cs
+++ AspireApp1/AspireApp1.ApiService/Program.cs
@@ -19,7 +19,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "hotter", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
 
 app.MapGet("/weatherforecast", () =>
 {
